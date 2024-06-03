4a453e01afa5b7f02ed7172d9280bd5196169541
JerryJTC <jtovarc3@uteq.edu.ec>
2024-06-03 12:02:16 -0500

    ffff

--- app/src/main/java/com/example/practica3/MainActivity.java
+++ app/src/main/java/com/example/practica3/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.practica3;
 
+import static android.app.ProgressDialog.show;
+
+import static java.lang.invoke.VarHandle.AccessMode.GET;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +15,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import WEBSERVICE.Asynchtask;
+import WEBSERVICE.WebService;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void clicklogin(View v){
+        Map<String, String> datos = new HashMap<String, String>();
+        EditText idCedula = findViewById(R.id.idCedula);
+        String usr = idCedula.getText().toString();
+        EditText idClave = findViewById(R.id.idClave);
+        String pass = idClave.getText().toString();
+        WebService ws= new WebService(
+                "https://revistas.uteq.edu.ec/ws/login.php?"+ "usr=" + usr + "&pass=" + pass
+
+                ,datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET");
+    }
+    @Override
+    public void processFinish(String result) throws JSONException {
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
     }
 }
